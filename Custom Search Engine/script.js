758478d5cd81479ee00d7b01734d2986f74a8ef0
Jia <yijh1705@gmail.com>
2023-05-25 21:40:21 -0400

    tried to finish "Custom Search Engine", did not work

---
+++ Custom Search Engine/script.js
@@ -0,0 +1,10 @@
+function hndlr(response) {
+    try {
+      document.getElementById('content').innerHTML += `<div style="color: grey;">Holy Moly! About ${response.searchInformation.formattedTotalResults} results in ${response.searchInformation.formattedSearchTime} seconds!</div>`
+      for (var i = 0; i < response.items.length; i++) {
+        document.getElementById("content").innerHTML += `<div style="align-items: center;"><br><a style="color: grey; font-size: 12px; text-decoration: none;" href=${response.items[i].link} target="_blank">${response.items[i].link}</a><a target="_blank" href=${response.items[i].link} style="text-decoration: none;"><h2 style="margin-top: 2px;">${response.items[i].title}</h2></a><div style="margin-top: -8px;">${response.items[i].htmlSnippet}</div></div>`;
+      }
+    } catch(error) {
+      document.getElementById('content').innerHTML = 'error!';
+    }
+  }

1b6106685fef1cba2502d0345d396f5434d5f598
Jia <yijh1705@gmail.com>
2023-05-26 00:56:26 -0400

    removed API key from Custom Search Engine

--- Custom Search Engine/query.js
+++ Custom Search Engine/query.js
@@ -3,7 +3,7 @@ function submit() {
     document.getElementById('content').innerHTML = '';
     var val = document.getElementById('search').value;
     var newelement = document.createElement('script');
-    newelement.src = `https://www.googleapis.com/customsearch/v1?key=AIzaSyAzL-cl7NeTTepsNPG7AlNqq3SDlJScZPc&cx=003606982592251140240:5xbiwoxb3m0&q=${val}&callback=hndlr`;
+    newelement.src = `https://www.googleapis.com/customsearch/v1?key=API_KEY&cx=003606982592251140240:5xbiwoxb3m0&q=${val}&callback=hndlr`;
     newelement.id = 'mainscript';
     document.body.appendChild(newelement);
   }
